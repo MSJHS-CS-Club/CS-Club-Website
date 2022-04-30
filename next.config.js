f7dd58fec583295354292eb1e558c61036d203cd
Shubham Patil <shubhampatilsd@gmail.com>
2022-04-29 23:14:59 -0700

    lint rules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-img-element": "off",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
