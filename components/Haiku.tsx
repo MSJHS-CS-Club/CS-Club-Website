16eb5e8c463a508cd7f445761f73dc79bb469d96
Shubham Patil <shubhampatilsd@gmail.com>
2023-08-10 13:50:24 -0700

    update some things

--- components/Haiku.tsx
+++ components/Haiku.tsx
@@ -1,13 +1,20 @@
-import styles from '../styles/Haiku.module.css';
+import styles from "../styles/Haiku.module.css";
 export const Haiku = () => {
-    return (
-        <div className={styles.haiku_vh}>
-            <div>
-                <div className={styles.haiku_line}>"Come to <span className={styles.green}>C16</span></div>
-                <div className={styles.haiku_line}>To <span className={styles.yellow}>code and learn together</span>,</div>
-                <div className={styles.haiku_line}>On <span className={styles.red}>Fridays</span> at <span className={styles.red}>Lunch</span>!"</div>
-                <p className={styles.haiku_note}>- A Haiku by the CS Club</p>
-            </div>
+  return (
+    <div className={styles.haiku_vh}>
+      <div>
+        <div className={styles.haiku_line}>
+          "Come to <span className={styles.green}>N3</span>
         </div>
-    );
+        <div className={styles.haiku_line}>
+          To <span className={styles.yellow}>code and learn together</span>,
+        </div>
+        <div className={styles.haiku_line}>
+          On <span className={styles.red}>Fridays</span> at{" "}
+          <span className={styles.red}>Lunch</span>!"
+        </div>
+        <p className={styles.haiku_note}>- A Haiku by the CS Club</p>
+      </div>
+    </div>
+  );
 };
